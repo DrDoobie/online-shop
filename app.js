f397eb1effaf14b21ef3e40a5f4be9b2d3cb9110
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-19 12:40:54 +0200

    Revert

--- app.js
+++
@@ -1,7 +0,0 @@
-function openSideBar() {
-    document.getElementById("sideBar").style.display = "block";
-}
-
-function closeSideBar() {
-    document.getElementById("sideBar").style.display = "none";
-}
