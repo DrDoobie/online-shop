d54b592d111354eaf73d3a81260cafff99dbb25d
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-19 12:35:37 +0200

    Started side bar

---
+++ app.js
@@ -0,0 +1,7 @@
+function openSideBar() {
+    document.getElementById("sideBar").style.display = "block";
+}
+
+function closeSideBar() {
+    document.getElementById("sideBar").style.display = "none";
+}
